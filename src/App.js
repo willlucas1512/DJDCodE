fb23c8e4648cdbeb3d7d1c44201f8a55b7bac2c8
willlucas1512 <willlucas1512@gmail.com>
2022-08-02 17:45:12 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { ScreenOrientation } from "./components";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux";
+import configureStore from "./redux/configureStore";
 
 function App() {
+  const xConfigureStore = configureStore();
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ScreenOrientation />{" "}
+    <Provider store={xConfigureStore.store}>
+      <PersistGate loading={null} persistor={xConfigureStore.persistor}>
+        <ScreenOrientation />
       </PersistGate>
     </Provider>
   );
