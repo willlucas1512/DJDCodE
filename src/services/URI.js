ecc88debde760f6ab7ca9b53a4903e2383bd4c68
William Lucas <41302062+willlucas1512@users.noreply.github.com>
2023-11-14 16:57:27 -0300

    Update URI.js

--- src/services/URI.js
+++ src/services/URI.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://574e-191-34-143-222.ngrok-free.app/";
+export const BASE_URL =
+  "https://acaf-2804-7f1-e787-9611-82a-70a8-1ef0-78b7.ngrok-free.app/";
 
 export const RESET_PASSWORD_URI = BASE_URL + "reset-pass/";
 export const RECOVER_PASSWORD_URI = BASE_URL + "recover/";
